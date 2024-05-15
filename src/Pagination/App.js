88a7b31b336efa7401b55d51683cd184795f41a9
bhagya37 <t.bhagyalaxmi3702@gamil.com>
2024-05-15 15:39:11 +0530

    updated-pagination

--- src/Pagination/App.js
+++ src/Pagination/App.js
@@ -1,95 +1,86 @@
-import React, { useState, useEffect } from "react";
-import User from "./User";
+import React ,{useState,useEffect} from "react";
 import './App.css'
 import axios from "axios";
-
-function App() {
-  const [user, setUser] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const recordsPerPage = 5;
-
-  useEffect(() => {
-    axios("https://dummyjson.com/users")
-      .then((res) => {
-        setUser(res.data.users);
-      })
-      .catch((err) => {
-        console.log("error");
-      });
-  }, []);
-
+function App(){
+  let[user,setUser] = useState([])
+  let[currentPage,setCurrentPage] = useState(1)
+  let recordsPerPage = 5;
+  useEffect(()=>{
+ axios.get("https://dummyjson.com/users")
+ .then((res)=>{
+  setUser(res.data.users)
+ })
+  },[])
   const lastIndex = currentPage * recordsPerPage;
-  const firstIndex = lastIndex - recordsPerPage;
-  const records = user.slice(firstIndex, lastIndex);
-  const totalPages = Math.ceil(user.length / recordsPerPage);
+  const firstIndex = currentPage * recordsPerPage - recordsPerPage;
+  const records = user.slice(firstIndex,lastIndex)
+  const totalPages = Math.ceil(user.length / recordsPerPage)
 
-  const changePage = (page) => {
-    setCurrentPage(page);
-  };
 
   const nextPage = () => {
-    if (currentPage < totalPages) {
-      setCurrentPage(currentPage + 1);
-    }
-  };
-
-  const prePage = () => {
-    if (currentPage > 1) {
-      setCurrentPage(currentPage - 1);
-    }
-  };
-
-  return (
+        if (currentPage < totalPages) {
+          setCurrentPage(currentPage + 1);
+        }
+      };
+    
+      const prePage = () => {
+        if (currentPage > 1) {
+          setCurrentPage(currentPage - 1);
+        }
+      };
+      const deleteUser = (id)=>{
+        const updatedUser= user.filter((user)=>user.id !==id)
+          setUser(updatedUser);
+        
+      }
+  return(
     <>
-      <div>
-        <table>
-          <thead>
-            <tr>
-              <th>Id</th>
-              <th>Name</th>
-              <th>Gender</th>
-              <th>Email</th>
-            </tr>
-          </thead>
-          <tbody>
-            {records.map((data, index) => (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <td>Id</td>
+            <td>Name</td>
+            <td>Gender</td>
+            <td>Email</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          {records.map((data, index)=>{
+            return(
               <tr key={index}>
                 <td>{data.id}</td>
                 <td>{data.firstName}</td>
                 <td>{data.gender}</td>
                 <td>{data.email}</td>
+                <td>
+                  <button>Edit</button>
+                  <button onClick={()=>deleteUser(data.id)}>Delete</button>
+                </td>
               </tr>
-            ))}
-          </tbody>
-        </table>
-        <nav >
-          <ul className="pagination">
-            <li className="page-item" activeClassName="active">
-              <button className="page-link1" onClick={prePage}>
-                Prev
-              </button>
-            </li>
-            {[...Array(totalPages).keys()].map((n, i) => (
-              <li
-                className={`page-item ${currentPage === n + 1 ? "active" : ""}`}
-                key={i}
-              >
-                <button className="page-link" onClick={() => changePage(n + 1)}>
-                  {n + 1}
-                </button>
-              </li>
-            ))}
-            <li className="page-item">
-              <button className="page-link1" onClick={nextPage}>
-                Next
-              </button>
-            </li>
-          </ul>
-        </nav>
-      </div>
-      <User setUser={setUser} />
-    </>
-  );
-}
+            )
+          })
 
+          }
+        </tbody>
+      </table>
+      <nav >
+           <ul className="pagination">
+             <li className="page-item" activeClassName="active">
+               <button className="page-link1" onClick={prePage}>
+                 Prev
+              </button>
+             </li>
+            <li className="page-item">
+               <button className="page-link1" onClick={nextPage}>
+                 Next
+               </button>
+             </li>
+           </ul>
+       </nav>
+    </div>
+    </>
+  )
+}
 export default App;
